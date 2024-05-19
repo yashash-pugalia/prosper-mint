ca8bdd0bfcd998d0ca76a6f8d4a5a3f6d4a47670
Yashash Pugalia <yashash1511@gmail.com>
2024-05-19 17:56:33 +0530

    Improved Portfolio page a bit

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	export let data;
 </script>
 
@@ -7,15 +9,22 @@
 </h1>
 
 <div class="flex gap-4">
-	<aside class="flex flex-col w-80 bg-base-200 min-h-96 rounded border p-2">
+	<aside class="flex flex-col w-96 bg-base-200 min-h-96 rounded border p-2 gap-2">
+		<div class="flex gap-2 items-center">
+			<Icon icon="material-symbols:account-balance-outline-rounded" class="text-xl" /> Banks
+			<a class="btn btn-xs ml-auto btn-ghost" href="/banks">See All</a>
+		</div>
+
 		{#each data.banks as bank}
 			<div class="border rounded p-2 flex gap-2 items-center bg-base-100">
 				<img class="w-8 h-8" src="/bank-logos/{bank.name}.png" alt="" />
 				<div class="grow">
-					<h2 class="flex gap-2 font-semibold">
-						{bank.name}
+					<h2 class="flex justify-between gap-2 font-semibold">
+						<span class="truncate shrink">
+							{bank.name}
+						</span>
 
-						<span class="ml-auto"
+						<span
 							>{bank.balance.toLocaleString('en-IN', {
 								style: 'currency',
 								currency: 'INR',
@@ -32,7 +41,13 @@
 		{/each}
 	</aside>
 
-	<main class="border p-2 rounded bg-base-200 grow">
+	<main class="border p-2 rounded bg-base-200 grow flex flex-col gap-2">
+		<div class="flex gap-2 items-center">
+			<Icon icon="material-symbols:featured-play-list-outline-rounded" class="text-xl" /> Recent Transactions
+
+			<a class="btn btn-xs btn-ghost ml-auto" href="/transactions">See All</a>
+		</div>
+
 		{#each data.transactions as transaction}
 			<div class="border rounded p-2 flex gap-2 items-center bg-base-100">
 				{transaction.amount.toLocaleString('en-IN', {
